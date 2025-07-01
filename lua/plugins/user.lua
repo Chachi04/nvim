2270c4661afca78223c623d150ea1d8ed7449a4d
Chachi <w.jiaqi004@gmail.com>
2025-07-01 17:26:50 +0200

    enable(snacks.scroll)
    smooth scrolling

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -19,6 +19,7 @@ return {
   -- customize dashboard options
   {
     "folke/snacks.nvim",
+    ---@type snacks.Config
     opts = {
       dashboard = {
         preset = {
@@ -36,6 +37,24 @@ return {
             "██   ████   ████   ██ ██      ██",
           }, "\n"),
         },
+      },
+      ---@type snacks.scroll.Config
+      scroll = {
+        enabled = true,
+        animate = {
+          duration = { step = 15, total = 250 },
+          easing = "linear",
+        },
+        animate_repeat = {
+          delay = 100,
+          duration = { step = 5, total = 50 },
+          easing = "linear",
+        },
+        filter = function(buf)
+          return vim.g.snacks_scroll ~= false
+            and vim.b[buf].snacks_scroll ~= false
+            and vim.bo[buf].buftype ~= "terminal"
+        end,
       },
     },
     keys = {
